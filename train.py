a8c44f5b7cb6d81067ff129b8726832388a86308
Atharva Moroney <moroneyatharva@gmail.com>
2022-11-19 17:13:58 -0500

    initial commit

--- train.py
+++ train.py
@@ -2,177 +2,193 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import pandas as pd
-import warnings
+import argparse
 
 from dataset import ImageGenDataset
 from model import Generator, Discriminator
 from loss import GeneratorLoss, DiscriminatorLoss
 from utils import weights_init, plot_images
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class GAN_Trainer():
+    def __init__(self, epochs, batch_size, lr, dataset_file, checkpoint_interval, print_interval, checkpoints_folder, results_folder):
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.lr = lr
+        self.print_interval = print_interval
+        self.dataset_file = dataset_file
+        self.checkpoints_folder = checkpoints_folder
+        self.checkpoint_interval = checkpoint_interval
+        self.results_folder = results_folder
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-warnings.filterwarnings("ignore", module="matplotlib\..*")
+        self.betas = (0.5, 0.999)
 
-batch_size = 128
-lr = 0.0002
-betas = (0.5, 0.999)
-print_interval = 25
-datasetFile = 'flowers.hdf5'
+        self.transform = transforms.Compose([
+            transforms.Resize(64),
+            transforms.CenterCrop(64),
+            #transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5],[0.5, 0.5, 0.5])
+        ])
 
-transform = transforms.Compose([
-    transforms.Resize(64),
-    transforms.CenterCrop(64),
-    #transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5],[0.5, 0.5, 0.5])
-])
+        self.train_dataset = ImageGenDataset(filename=self.dataset_file, split='train', transform=self.transform)
+        self.val_dataset = ImageGenDataset(filename=self.dataset_file, split='valid', transform=self.transform)
 
-train_dataset = ImageGenDataset(filename=datasetFile, split='train', transform=transform)
-val_dataset = ImageGenDataset(filename=datasetFile, split='valid', transform=transform)
+        self.train_loader = DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.val_loader = DataLoader(dataset=self.val_dataset, batch_size=self.batch_size, shuffle=True)
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=True)
+        self.generator = Generator().to(self.device)
+        self.discriminator = Discriminator().to(self.device)
 
-generator = Generator().to(device)
-discriminator = Discriminator().to(device)
+        self.generator.apply(weights_init)
+        self.discriminator.apply(weights_init)
 
-generator.apply(weights_init)
-discriminator.apply(weights_init)
+        self.generator_optimizer = torch.optim.Adam(self.generator.parameters(), lr=self.lr, betas=self.betas)
+        self.discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=self.lr, betas=self.betas)
 
-generator_optimizer = torch.optim.Adam(generator.parameters(), lr=lr, betas=betas)
-discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
+        self.generator_loss = GeneratorLoss()
+        self.discriminator_loss = DiscriminatorLoss()
 
-generator_loss = GeneratorLoss()
-discriminator_loss = DiscriminatorLoss()
+    def train_epoch(self, epoch):
+        self.generator.train()
+        self.discriminator.train()
 
+        train_loss_g_acc = 0
+        train_loss_d_acc = 0
+        d_x_acc = 0
+        d_g_x_acc = 0
+        plotted = False
 
+        for batch_idx, data in enumerate(self.train_loader):
+            right_txt_embeds = data['right_txt_embeds'].to(self.device)
+            wrong_txt_embeds = data['wrong_txt_embeds'].to(self.device)
+            real_img = data['real_img'].to(self.device)
 
-def train_epoch(epoch):
-    generator.train()
-    discriminator.train()
+            noise = torch.randn(real_img.shape[0], 100, 1, 1).to(self.device)
 
-    train_loss_g_acc = 0
-    train_loss_d_acc = 0
-    d_x_acc = 0
-    d_g_x_acc = 0
-    plotted = False
+            self.discriminator_optimizer.zero_grad()
+            fake_img = self.generator(noise, right_txt_embeds)
+            Sr = self.discriminator(real_img, right_txt_embeds)  # real image, right text
+            Sw = self.discriminator(real_img, wrong_txt_embeds)  # real image, wrong text
+            Sf1 = self.discriminator(fake_img, right_txt_embeds)  # fake image, right text
+            loss_d = self.discriminator_loss(Sr, Sw, Sf1)
+            loss_d.backward()
+            self.discriminator_optimizer.step()
 
-    for batch_idx, data in enumerate(train_loader):
-        right_txt_embeds = data['right_txt_embeds'].to(device)
-        wrong_txt_embeds = data['wrong_txt_embeds'].to(device)
-        real_img = data['real_img'].to(device)
+            self.generator_optimizer.zero_grad()
+            fake_img = self.generator(noise, right_txt_embeds)
+            Sf2 = self.discriminator(fake_img, right_txt_embeds) 
+            loss_g = self.generator_loss(Sf2)
+            loss_g.backward()
+            self.generator_optimizer.step()
 
-        noise = torch.randn(real_img.shape[0], 100, 1, 1).to(device)
-
-        discriminator_optimizer.zero_grad()
-        fake_img = generator(noise, right_txt_embeds)
-        Sr = discriminator(real_img, right_txt_embeds)  # real image, right text
-        Sw = discriminator(real_img, wrong_txt_embeds)  # real image, wrong text
-        Sf1 = discriminator(fake_img, right_txt_embeds)  # fake image, right text
-        loss_d = discriminator_loss(Sr, Sw, Sf1)
-        loss_d.backward()
-        discriminator_optimizer.step()
-
-        generator_optimizer.zero_grad()
-        fake_img = generator(noise, right_txt_embeds)
-        Sf2 = discriminator(fake_img, right_txt_embeds) 
-        loss_g = generator_loss(Sf2)
-        loss_g.backward()
-        generator_optimizer.step()
-
-        if batch_idx%print_interval == 0:
-            print('Epoch', epoch, 'batch_idx', batch_idx, ': loss_d =', loss_d.item(), 'loss_g =', loss_g.item(), 'D(X) =', Sr.mean().item(), 'D(G(X)) =', Sf1.mean().item(), flush=True)
-
-        if not plotted:
-            plotted = True
-            plot_images(fake_img, generated=True, train=True, epoch=epoch, batch_idx=batch_idx)
-            plot_images(real_img, generated=False, train=True, epoch=epoch, batch_idx=batch_idx)
-            
-            train_loss_g_acc += loss_g.item()*right_txt_embeds.shape[0]
-            train_loss_d_acc += loss_d.item()*right_txt_embeds.shape[0]
-            d_x_acc += Sr.sum().item()
-            d_g_x_acc += Sf1.sum().item()
-
-    train_loss_g_acc /= len(train_loader.dataset)
-    train_loss_d_acc /= len(train_loader.dataset)
-    d_x_acc /= len(train_loader.dataset)
-    d_g_x_acc /= len(train_loader.dataset)
-
-    return train_loss_g_acc, train_loss_d_acc, d_x_acc, d_g_x_acc
-
-def validate(epoch):
-    generator.eval()
-    discriminator.eval()
-
-    val_loss_g_acc = 0
-    val_loss_d_acc = 0
-    d_x_acc = 0
-    d_g_x_acc = 0
-    plotted = False
-
-    with torch.no_grad():
-        for batch_idx, data in enumerate(val_loader):
-            right_txt_embeds = data['right_txt_embeds'].to(device)
-            wrong_txt_embeds = data['wrong_txt_embeds'].to(device)
-            real_img = data['real_img'].to(device)
-
-            noise = torch.randn(real_img.shape[0], 100, 1, 1).to(device)
-            
-            fake_img = generator(noise, right_txt_embeds)
-            Sr = discriminator(real_img, right_txt_embeds)  # real image, right text
-            Sw = discriminator(real_img, wrong_txt_embeds)  # real image, wrong text
-            Sf = discriminator(fake_img, right_txt_embeds)  # fake image, right text
-            loss_d = discriminator_loss(Sr, Sw, Sf)
-            loss_g = generator_loss(Sf)
-
-            val_loss_g_acc += loss_g.item()*right_txt_embeds.shape[0]
-            val_loss_d_acc += loss_d.item()*right_txt_embeds.shape[0]
-            d_x_acc += Sr.sum().item()
-            d_g_x_acc += Sf.sum().item()
+            if batch_idx % self.print_interval == 0:
+                print('Epoch', epoch, 'batch_idx', batch_idx, ': loss_d =', loss_d.item(), 'loss_g =', loss_g.item(), 'D(X) =', Sr.mean().item(), 'D(G(X)) =', Sf1.mean().item(), flush=True)
 
             if not plotted:
                 plotted = True
-                plot_images(fake_img, generated=True, train=False, epoch=epoch, batch_idx=batch_idx)
-                plot_images(real_img, generated=False, train=False, epoch=epoch, batch_idx=batch_idx)
+                plot_images(fake_img, generated=True, train=True, epoch=epoch, batch_idx=batch_idx)
+                plot_images(real_img, generated=False, train=True, epoch=epoch, batch_idx=batch_idx)
+                
+                train_loss_g_acc += loss_g.item()*right_txt_embeds.shape[0]
+                train_loss_d_acc += loss_d.item()*right_txt_embeds.shape[0]
+                d_x_acc += Sr.sum().item()
+                d_g_x_acc += Sf1.sum().item()
 
-    val_loss_g_acc /= len(train_loader.dataset)
-    val_loss_d_acc /= len(train_loader.dataset)
-    d_x_acc /= len(train_loader.dataset)
-    d_g_x_acc /= len(train_loader.dataset)
+        train_loss_g_acc /= len(self.train_loader.dataset)
+        train_loss_d_acc /= len(self.train_loader.dataset)
+        d_x_acc /= len(self.train_loader.dataset)
+        d_g_x_acc /= len(self.train_loader.dataset)
 
-    return val_loss_g_acc, val_loss_d_acc, d_x_acc, d_g_x_acc
+        return train_loss_g_acc, train_loss_d_acc, d_x_acc, d_g_x_acc
 
-def train(epochs):
+    def validate(self, epoch):
+        self.generator.eval()
+        self.discriminator.eval()
 
-    train_stats = pd.DataFrame(columns=['epoch', 'loss_d', 'loss_g', 'D(X)', 'D(G(X))'])
-    val_stats = pd.DataFrame(columns=['epoch', 'loss_d', 'loss_g', 'D(X)', 'D(G(X))'])
+        val_loss_g_acc = 0
+        val_loss_d_acc = 0
+        d_x_acc = 0
+        d_g_x_acc = 0
+        plotted = False
 
-    for epoch in range(1, epochs+1):
-        train_loss_g_acc, train_loss_d_acc, train_d_x_acc, train_d_g_x_acc = train_epoch(epoch)
-        val_loss_g_acc, val_loss_d_acc, val_d_x_acc, val_d_g_x_acc = validate(epoch)
-        
-        print('\nEpoch', epoch, ': Train loss_d =', train_loss_d_acc, 'loss_g =', train_loss_g_acc, 'D(X) =', train_d_x_acc, 'D(G(X)) =', train_d_g_x_acc, flush=True)
-        print('Epoch', epoch, ': Val loss_d =', val_loss_d_acc, 'loss_g =', val_loss_g_acc, 'D(X) =', val_d_x_acc, 'D(G(X)) =', val_d_g_x_acc, '\n', flush=True)
+        with torch.no_grad():
+            for batch_idx, data in enumerate(self.val_loader):
+                right_txt_embeds = data['right_txt_embeds'].to(self.device)
+                wrong_txt_embeds = data['wrong_txt_embeds'].to(self.device)
+                real_img = data['real_img'].to(self.device)
 
-        train_stats = train_stats.append({'epoch': epoch, 'loss_d': train_loss_d_acc, 'loss_g': train_loss_g_acc, 'D(X)': train_d_x_acc, 'D(G(X))': train_d_g_x_acc}, ignore_index=True)
-        val_stats = val_stats.append({'epoch': epoch, 'loss_d': val_loss_d_acc, 'loss_g': val_loss_g_acc, 'D(X)': val_d_x_acc, 'D(G(X))': val_d_g_x_acc}, ignore_index=True)
+                noise = torch.randn(real_img.shape[0], 100, 1, 1).to(self.device)
+                
+                fake_img = self.generator(noise, right_txt_embeds)
+                Sr = self.discriminator(real_img, right_txt_embeds)  # real image, right text
+                Sw = self.discriminator(real_img, wrong_txt_embeds)  # real image, wrong text
+                Sf = self.discriminator(fake_img, right_txt_embeds)  # fake image, right text
+                loss_d = self.discriminator_loss(Sr, Sw, Sf)
+                loss_g = self.generator_loss(Sf)
 
-        train_stats.to_csv('train_stats.csv')
-        val_stats.to_csv('val_stats.csv')
+                val_loss_g_acc += loss_g.item()*right_txt_embeds.shape[0]
+                val_loss_d_acc += loss_d.item()*right_txt_embeds.shape[0]
+                d_x_acc += Sr.sum().item()
+                d_g_x_acc += Sf.sum().item()
 
-        if epoch%10 == 0 or epoch == 1:
-            checkpoint = {"generator": generator.state_dict(),
-                            "discriminator": discriminator.state_dict(),
-                            "generator_optimizer": generator_optimizer.state_dict(),
-                            "discriminator_optimizer": discriminator_optimizer.state_dict(),
-                            "epoch": epoch}
-        
-            torch.save(checkpoint, "checkpoints/checkpoint_epoch_" + str(epoch) + ".pth")
+                if not plotted:
+                    plotted = True
+                    plot_images(results_folder=self.results_folder, images=fake_img, generated=True, train=False, epoch=epoch, batch_idx=batch_idx)
+                    plot_images(results_folder=self.results_folder, images=real_img, generated=False, train=False, epoch=epoch, batch_idx=batch_idx)
+
+        val_loss_g_acc /= len(self.train_loader.dataset)
+        val_loss_d_acc /= len(self.train_loader.dataset)
+        d_x_acc /= len(self.train_loader.dataset)
+        d_g_x_acc /= len(self.train_loader.dataset)
+
+        return val_loss_g_acc, val_loss_d_acc, d_x_acc, d_g_x_acc
+
+    def train(self):
+
+        train_stats = pd.DataFrame(columns=['epoch', 'loss_d', 'loss_g', 'D(X)', 'D(G(X))'])
+        val_stats = pd.DataFrame(columns=['epoch', 'loss_d', 'loss_g', 'D(X)', 'D(G(X))'])
+
+        for epoch in range(1, self.epochs+1):
+            train_loss_g_acc, train_loss_d_acc, train_d_x_acc, train_d_g_x_acc = self.train_epoch(epoch)
+            val_loss_g_acc, val_loss_d_acc, val_d_x_acc, val_d_g_x_acc = self.validate(epoch)
+            
+            print('\nEpoch', epoch, ': Train loss_d =', train_loss_d_acc, 'loss_g =', train_loss_g_acc, 'D(X) =', train_d_x_acc, 'D(G(X)) =', train_d_g_x_acc, flush=True)
+            print('Epoch', epoch, ': Val loss_d =', val_loss_d_acc, 'loss_g =', val_loss_g_acc, 'D(X) =', val_d_x_acc, 'D(G(X)) =', val_d_g_x_acc, '\n', flush=True)
+
+            train_stats = train_stats.append({'epoch': epoch, 'loss_d': train_loss_d_acc, 'loss_g': train_loss_g_acc, 'D(X)': train_d_x_acc, 'D(G(X))': train_d_g_x_acc}, ignore_index=True)
+            val_stats = val_stats.append({'epoch': epoch, 'loss_d': val_loss_d_acc, 'loss_g': val_loss_g_acc, 'D(X)': val_d_x_acc, 'D(G(X))': val_d_g_x_acc}, ignore_index=True)
+
+            train_stats.to_csv('train_stats.csv')
+            val_stats.to_csv('val_stats.csv')
+
+            if epoch % self.checkpoint_interval == 0 or epoch == 1:
+                checkpoint = {"generator": self.generator.state_dict(),
+                                "discriminator": self.discriminator.state_dict(),
+                                "generator_optimizer": self.generator_optimizer.state_dict(),
+                                "discriminator_optimizer": self.discriminator_optimizer.state_dict(),
+                                "epoch": epoch}
+            
+                torch.save(checkpoint, self.checkpoints_folder + "/checkpoint_epoch_" + str(epoch) + ".pth")
 
 
+parser = argparse.ArgumentParser(description='Train GAN')
+parser.add_argument('--epochs', default=200)
+parser.add_argument('--batch_size', default=128)
+parser.add_argument('--learning_rate', default=0.0002)
+parser.add_argument('--dataset_file', default='flowers.hdf5')
+parser.add_argument('--print_interval', default=25)
+parser.add_argument('--checkpoints_folder', default='checkpoints')
+parser.add_argument('--checkpoint_interval', default=10)
+parser.add_argument('--results_folder', default='results')
 
 if __name__ == '__main__':
-    train(200)
+    args = parser.parse_args()
+
+    trainer = GAN_Trainer(args.epochs, args.batch_size, args.learning_rate, args.dataset_file, args.checkpoint_interval, args.print_interval, args.checkpoints_folder, args.results_folder)
+    trainer.train()
+
+    print('Training completed!')
 
 
 
--- utils.py
+++ utils.py
@@ -1,28 +1,29 @@
 import torch.nn as nn
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
+import numpy as np
 
 def weights_init(m):
-  classname = m.__class__.__name__
-  if classname.find('Conv') != -1:
-    nn.init.normal_(m.weight.data, 0.0, 0.02)
-  elif classname.find('BatchNorm') != -1:
-    nn.init.normal_(m.weight.data, 1.0, 0.02)
-    nn.init.constant_(m.bias.data, 0)
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
-def plot_images(images, generated, train, epoch, batch_idx):
-  images = images.cpu()
-  im = make_grid(images, nrow=8, pad_value=1)  # the default nrow is 8
-  plt.figure(figsize=(10,10))
-  plt.axis("off")
-  plt.title("Generated Images" if generated else "Real Images")
+def plot_images(results_folder, images, generated, train, epoch, batch_idx):
+    images = images.cpu()
+    im = make_grid(images, nrow=8, pad_value=1)  # the default nrow is 8
+    plt.figure(figsize=(10,10))
+    plt.axis("off")
+    plt.title("Generated Images" if generated else "Real Images")
 
-  filename = 'results/'
-  filename += ('train/' if train else 'val/')
-  filename += ('generated/' if generated else 'real/')
-  filename += str(epoch) + '_' + str(batch_idx) + '.jpg'
+    filename = results_folder
+    filename += ('/train/' if train else 'val/')
+    filename += ('/generated/' if generated else 'real/')
+    filename += str(epoch) + '_' + str(batch_idx) + '.jpg'
 
-  # We need to transpose the images from CWH to WHC
-  im = np.transpose(im.numpy(), (1, 2, 0)).clip(0, 1)
-  plt.imshow(im)
-  plt.savefig(filename)
+    # We need to transpose the images from CWH to WHC
+    im = np.transpose(im.numpy(), (1, 2, 0)).clip(0, 1)
+    plt.imshow(im)
+    plt.savefig(filename)
